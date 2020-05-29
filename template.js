9f8125d475c35703928b2dd596d48ab1f083855b
alexarirok <akorir233@gmail.com>
2020-05-29 15:10:01 -0700

    working on backend

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+    return `<!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
+                <title>Mern Skelee</title>
+            </head>
+            <body>
+                <div id="root"> Hello World <p>Hi everyone welcome home</p> </div>
+            </body>
+        </html>`
+}
